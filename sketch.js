de98e2688d0f73a42be9c32b0105039d0939056d
KuldeepSinghGurjar2153 <86063787+KuldeepSinghGurjar2153@users.noreply.github.com>
2021-10-06 19:27:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,170 +1,163 @@
-//Game States
-var PLAY=1;
-var END=0;
-var gameState=1;
-var score;
+/*--------------------------------------------------------*/
+var PLAY = 1;
+var END = 0;
+var WIN = 2;
+var gameState = PLAY;
 
-var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
-var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
+var kangaroo, kangaroo_running , kangaroo_collided;
+var jungle, invisiblejungle;
 
+var obstaclesGroup, obstacle1;
 
+var score=0;
 
-var gameoverSound, knifeSwooshSound;
+var gameOver, restart;
 
 function preload(){
-  
-  knifeImage = loadImage("knife.png");
-  monsterImage = loadAnimation("alien1.png","alien2.png")
-  fruit1 = loadImage("fruit1.png");
-  fruit2 = loadImage("fruit2.png");
-  fruit3 = loadImage("fruit3.png");
-  fruit4 = loadImage("fruit4.png");
-  gameOverImage = loadImage("gameover.png");
-
-  gameoverSound = loadSound("gameover.mp3");
-  knifeSwooshSound = loadSound("knifeSwoosh.mp3");
-
-  //load sound here
+  kangaroo_running =   loadAnimation("assets/kangaroo1.png","assets/kangaroo2.png","assets/kangaroo3.png");
+  kangaroo_collided = loadAnimation("assets/kangaroo1.png");
+  jungleImage = loadImage("assets/bg.png");
+  shrub1 = loadImage("assets/shrub1.png");
+  shrub2 = loadImage("assets/shrub2.png");
+  shrub3 = loadImage("assets/shrub3.png");
+  obstacle1 = loadImage("assets/stone.png");
+  gameOverImg = loadImage("assets/gameOver.png");
+  restartImg = loadImage("assets/restart.png");
+  jumpSound = loadSound("assets/jump.wav");
+  collidedSound = loadSound("assets/collided.wav");
 }
 
-
-
 function setup() {
-  createCanvas(600, 600);
-  
-  //creating sword
-   knife=createSprite(40,200,20,20);
-   knife.addImage(knifeImage);
-   knife.scale=0.7
-  
-  //set collider for sword
-  knife.setCollider("rectangle",0,0,40,40);
+  createCanvas(800, 400);
 
-  // Score variables and Groups
-  score=0;
-  fruitGroup=createGroup();
-  monsterGroup=createGroup();
+  jungle = createSprite(400,100,400,20);
+  jungle.addImage("jungle",jungleImage);
+  jungle.scale=0.3
+  jungle.x = width /2;
 
+  /*Create a sprite for the kangaroo:
+Apply Kangaroo_running and kangaroo_collided animation to it. Scale the kangaroo if required.
+Set the collider of the kangaroo as a circle and the radius of the collider as 300
+*/
+//Create a sprite for Invisible ground and place it at the bottom side of the screen.
 
+ kangaroo = createSprite(20,20,100,100)
+ kangaroo.addAnimation("kangaroo",kangaroo_running,kangaroo_collided);
+ kangaroo.scale=0.03;
+
+ 
 
   
+  shrubsGroup = new Group();
+  obstaclesGroup = new Group();
+
+  kangaroo.setCollider("circle",0,0,40);
+  kangaroo.debug = true
   
+  score = 0;
+
 }
 
 function draw() {
-  background("lightblue");
+  background(255);
   
+//In function draw(), Set the x-Position of the kangaroo according to the camera    
+  if (gameState===PLAY){
 
-  
-  if(gameState===PLAY){
-    
-    //Call fruits and Monster function
-    fruits();
-    Monster();
-    
-    // Move sword with mouse
-    knife.y=World.mouseY;
-    knife.x=World.mouseX;
+    jungle.velocityX=-3
 
-    monsterGroup.velocityX=-(8+(score/2));
-    fruitGroup.velocityX=-(8+(score/2));
-  
-    // Increase score if sword touching fruit
-    if(fruitGroup.isTouching(knife)){
-      fruitGroup.destroyEach();
-      knifeSwooshSound.play()
-     score=score+1;
-    }
-    else
+    if(jungle.x<100)
     {
-      // Go to end state if sword touching enemy
-      if(monsterGroup.isTouching(knife)){
-        gameState=END;
-        
-        //add gameover sound here
-        
-        fruitGroup.destroyEach();
-        monsterGroup.destroyEach();
-        fruitGroup.setVelocityXEach(0);
-        monsterGroup.setVelocityXEach(0);
-        
-        // Change the animation of sword to gameover and reset its position
-        knife.addImage(gameOverImage);
-        knife.scale=2;
-        knife.x=300;
-        knife.y=300;
-         gameoverSound.play()
-
-      }
+       jungle.x=400
     }
-      if(fruitGroup.isTouching(knife)){
-        score=score+2;
-
-      }
-
-  }
+   console.log(kangaroo.y)
+    if(keyDown("space")&& kangaroo.y>270) {
+      jumpSound.play();
+      kangaroo.velocityY = -16;
+    }
   
+    kangaroo.velocityY = kangaroo.velocityY + 0.8
+    spawnShrubs();
+    spawnObstacles();
+
+    kangaroo.collide(invisibleGround);
+    
+    if(obstaclesGroup.isTouching(kangaroo)){
+      collidedSound.play();
+      gameState = END;
+    }
+    if(shrubsGroup.isTouching(kangaroo)){
+
+      shrubsGroup.destroyEach();
+    }
+  }
+  else if (gameState === END) {
+    //set velcity of each game object to 0
+    kangaroo.velocityY = 0;
+    jungle.velocityX = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    shrubsGroup.setVelocityXEach(0);
+
+    //change the kangaroo animation
+    kangaroo.changeAnimation("collided",kangaroo_collided);
+    
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    shrubsGroup.setLifetimeEach(-1);
+    
+  }
+
+  kangaroo.collide(invisibleGround);
   drawSprites();
-  //Display score
-  textSize(25);
-  text("Score : "+ score,250,50);
+
+
 }
 
+function spawnShrubs() {
+  //write code here to spawn the clouds
+  if (frameCount % 150 === 0) {
 
-function Monster(){
-  if(World.frameCount%200===0){
-    monster=createSprite(400,200,20,20);
-    monster.addAnimation("moving", monsterImage);
-    monster.y=Math.round(random(100,550));
-    //update below give line of code for increase monsterGroup speed by 10
-    monster.velocityX = -8;
-    monster.setLifetime=50;
+//Set the x-position of the shrub according to the game camera.
+    shrub.velocityX = -(6 + 3*score/100)
+    shrub.scale = 0.6;
+
+    var rand = Math.round(random(1,3));
+    switch(rand) {
+      case 1: shrub.addImage(shrub1);
+              break;
+      case 2: shrub.addImage(shrub2);
+              break;
+      case 3: shrub.addImage(shrub3);
+              break;
+      default: break;
+    }
     
-    monsterGroup.add(monster);
+    //assign scale and lifetime to the shrub           
+    shrub.scale = 0.05;
+     //assign lifetime to the variable
+    shrub.lifetime = 400;
+    
+    shrub.setCollider("rectangle",0,0,shrub.width/2,shrub.height/2)
+    //add each cloud to the group
+    shrubsGroup.add(shrub);
+    
   }
+  
 }
 
-function fruits(){
-  if(World.frameCount%80===0){
-    position = Math.round(random(1,2));
-    fruit=createSprite(400,200,20,20);
+function spawnObstacles() {
+  if(frameCount % 120 === 0) {
+
+    var obstacle = createSprite(camera.position.x+400,330,40,40);
+    obstacle.setCollider("rectangle",0,0,200,200)
+    obstacle.addImage(obstacle1);
+    obstacle.velocityX = -(6 + 3*score/100)
+    obstacle.scale = 0.15;
+    //assign scale and lifetime to the obstacle           
+ 
+    obstacle.lifetime = 400;
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
     
-     //using random variable change the position of fruit, to make it more challenging
-    
-    if(position==1)
-    {
-    fruit.x=600;
-    //update below give line of code for increase fruitGroup speed by 4
-    fruit.velocityX=-7
-    }
-    else
-    {
-      if(position==2){
-      fruit.x=0;
-      
-     //update below give line of code for increase fruitGroup speed by 4
-      fruit.velocityX= 7;
-      }
-    }
-    
-    fruit.scale=0.2;
-     //fruit.debug=true;
-     r=Math.round(random(1,4));
-    if (r == 1) {
-      fruit.addImage(fruit1);
-    } else if (r == 2) {
-      fruit.addImage(fruit2);
-    } else if (r == 3) {
-      fruit.addImage(fruit3);
-    } else {
-      fruit.addImage(fruit4);
-    }
-    
-    fruit.y=Math.round(random(50,550));
-   
-    
-    fruit.setLifetime=100;
-    
-    fruitGroup.add(fruit);
   }
 }
